6ddd0516741f7db4326545b32e588e1841bd9ac2
coolcyrstal <chayenjr.sk131@gmail.com>
2017-01-18 16:42:50 +0700

    create sign in to firebase user by email address

--- app/src/main/java/com/example/chayen/cookingsupporter/LoginRegister.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/LoginRegister.java
@@ -3,19 +3,31 @@ package com.example.chayen.cookingsupporter;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chayen.cookingsupporter.CreateAccount.RegisterPage;
 import com.example.chayen.cookingsupporter.MainPage.HomePage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginRegister extends AppCompatActivity {
 
-    EditText text_user_id, text_password;
+    EditText text_email, text_password;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +36,10 @@ public class LoginRegister extends AppCompatActivity {
 
         TextView create_account = (TextView)findViewById(R.id.create_new_account);
         Button login_button = (Button)findViewById(R.id.login_button);
-        text_user_id = (EditText)findViewById(R.id.textUserId);
-        text_password = (EditText)findViewById(R.id.textPassword);
+        text_email = (EditText)findViewById(R.id.userEmail);
+        text_password = (EditText)findViewById(R.id.userPassword);
+
+        mAuth = FirebaseAuth.getInstance();
 
         create_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,14 +52,56 @@ public class LoginRegister extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(text_user_id.getText().toString().equals("") || text_password.getText().toString().equals("")){
+                if(text_email.getText().toString().equals("") || text_password.getText().toString().equals("")){
                     checkLoginInfo(LoginRegister.this, "User_Id or Password incorrect", "Please type again.", "OK");
                 } else{
-                    Intent intent = new Intent(LoginRegister.this, HomePage.class);
-                    startActivity(intent);
+                    loginWithEmailPass();
                 }
             }
         });
+    }
+
+    protected void loginWithEmailPass(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d("signed in", "onAuthStateChanged:signed_in:" + user.getUid());
+                }
+                else{
+                    Log.d("signed out", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mAuth.signInWithEmailAndPassword(text_email.getText().toString(), text_password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete( Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginRegister.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Intent intent = new Intent(LoginRegister.this, HomePage.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private AlertDialog checkLoginInfo(final AppCompatActivity act, CharSequence title,
