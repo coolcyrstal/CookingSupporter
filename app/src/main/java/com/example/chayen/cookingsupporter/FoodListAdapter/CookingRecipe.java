55103074ebff485417179cf4dd0a4f3cabf7640b
coolcyrstal <chayenjr.sk131@gmail.com>
2017-01-30 00:47:44 +0700

    edit cooking_recipe layout but have some detail bug --> listview on cooking method slow scrolling

--- app/src/main/java/com/example/chayen/cookingsupporter/FoodListAdapter/CookingRecipe.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/FoodListAdapter/CookingRecipe.java
@@ -1,7 +1,11 @@
 package com.example.chayen.cookingsupporter.FoodListAdapter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -17,45 +21,59 @@ import static com.example.chayen.cookingsupporter.MainPage.MainHomePageFragment.
 
 public class CookingRecipe extends AppCompatActivity {
 
-    ImageView cooking_recipe_image;
-    TextView cooking_recipe_name, cooking_recipe_type;
-    ArrayList<String> cooking_recipe_ingredient, cooking_recipe_method;
-    ListAdapter cooking_recipe_ingredientAdapter, cooking_recipe_methodAdapter;
-    ListView cooking_recipe_ingredientList, cooking_recipe_methodList;
     static int cooking_position;
+    ViewHolder holder = new ViewHolder();
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooking_recipe);
         initInstance();
+        renderRecipe();
     }
 
     public static void getFoodRecipePosition(int position){
         cooking_position = position;
     }
 
+    static class ViewHolder {
+        TextView cooking_recipe_name;
+        TextView cooking_recipe_type;
+        ArrayList<String> cooking_recipe_ingredient;
+        ArrayList<String> cooking_recipe_method;
+        ImageView cooking_recipe_image;
+        ListView cooking_recipe_ingredientList;
+        ListView cooking_recipe_methodList;
+        ListAdapter cooking_recipe_ingredientAdapter;
+        ListAdapter cooking_recipe_methodAdapter;
+    }
+
     private void initInstance(){
-        cooking_recipe_image = (ImageView)findViewById(R.id.cooking_recipe_foodimage);
-        cooking_recipe_name = (TextView)findViewById(R.id.cooking_recipe_foodname);
-        cooking_recipe_type = (TextView)findViewById(R.id.cooking_recipe_foodtype);
+        if(view == null){
+            holder.cooking_recipe_image = (ImageView)findViewById(R.id.cooking_recipe_foodimage);
+            holder.cooking_recipe_name = (TextView)findViewById(R.id.cooking_recipe_foodname);
+            holder.cooking_recipe_type = (TextView)findViewById(R.id.cooking_recipe_foodtype);
 
-        cooking_recipe_ingredientAdapter = new ArrayAdapter<String>(this, R.layout.cooking_recipe_adapter,
-                foodlist.get(cooking_position).getIngredient());
-        cooking_recipe_methodAdapter = new ArrayAdapter<String>(this, R.layout.cooking_recipe_adapter,
-                foodlist.get(cooking_position).getCooking_method());
+            holder.cooking_recipe_ingredientAdapter = new ArrayAdapter<String>(this, R.layout.cooking_recipe_adapter,
+                    foodlist.get(cooking_position).getIngredient());
+            holder.cooking_recipe_methodAdapter = new ArrayAdapter<String>(this, R.layout.cooking_recipe_adapter,
+                    foodlist.get(cooking_position).getCooking_method());
 
-        cooking_recipe_ingredientList = (ListView)findViewById(R.id.cooking_recipe_ingredient_list);
-        cooking_recipe_methodList = (ListView)findViewById(R.id.cooking_recipe_method_list);
+            holder.cooking_recipe_ingredientList = (ListView)findViewById(R.id.cooking_recipe_ingredient_list);
+            holder.cooking_recipe_methodList = (ListView)findViewById(R.id.cooking_recipe_method_list);
+//            view.setTag(holder);
+        }else{
+//            holder = (ViewHolder)view.getTag();
+        }
 
-        renderRecipe();
     }
 
     private void renderRecipe(){
-        Picasso.with(getApplicationContext()).load(foodlist.get(cooking_position).getFood_image()).into(cooking_recipe_image);
-        cooking_recipe_name.setText(foodlist.get(cooking_position).getFood_name());
-        cooking_recipe_type.setText(foodlist.get(cooking_position).getFood_type());
-        cooking_recipe_ingredientList.setAdapter(cooking_recipe_ingredientAdapter);
-        cooking_recipe_methodList.setAdapter(cooking_recipe_methodAdapter);
+        Picasso.with(getApplicationContext()).load(foodlist.get(cooking_position).getFood_image()).into(holder.cooking_recipe_image);
+        holder.cooking_recipe_name.setText(foodlist.get(cooking_position).getFood_name());
+        holder.cooking_recipe_type.setText(foodlist.get(cooking_position).getFood_type());
+        holder.cooking_recipe_ingredientList.setAdapter(holder.cooking_recipe_ingredientAdapter);
+        holder.cooking_recipe_methodList.setAdapter(holder.cooking_recipe_methodAdapter);
     }
 }
