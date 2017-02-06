28fc7623c98a0de59519396f4fd8ea434dac7eb1
coolcyrstal <chayenjr.sk131@gmail.com>
2017-02-07 02:03:19 +0700

    intent to upload image from gallery bug issue: user null on update

--- app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Profile.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Profile.java
@@ -2,12 +2,18 @@ package com.example.chayen.cookingsupporter.NavigationAndSearch;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.chayen.cookingsupporter.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,11 +21,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.squareup.picasso.Picasso;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Profile extends AppCompatActivity {
 
     Button upload_image, update_user_profile;
     EditText text_display_name;
+    ImageView profile_image;
+    Uri selectedImage;
+    Bitmap bitmap;
+    int select_image = 1; //code number for return result
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +44,10 @@ public class Profile extends AppCompatActivity {
     }
 
     private void initialize(){
-        upload_image = (Button)findViewById(R.id.user_profile_update_button);
+        upload_image = (Button)findViewById(R.id.upload_image_button);
         update_user_profile = (Button)findViewById(R.id.user_profile_update_button);
         text_display_name = (EditText)findViewById(R.id.text_updateDisplatName);
+        profile_image = (ImageView)findViewById(R.id.profile_image);
 
         update_user_profile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,9 +63,44 @@ public class Profile extends AppCompatActivity {
         upload_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                loadImageFromGallery();
             }
         });
+    }
+
+    private void loadImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), select_image);
+//        Intent intent_gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(intent_gallery, select_image);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == select_image && resultCode == RESULT_OK){
+            selectedImage = data.getData();
+//            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+//
+//            //read position of selected image
+//            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+//            cursor.moveToFirst();
+//
+//            //file and path of image
+//            int columnindex = cursor.getColumnIndex(filePathColumn[0]);
+//            String pathfile = cursor.getString(columnindex);
+//            cursor.close();
+
+//            Log.d("imageURI", ""+selectedImage);
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+//                profile_image.setImageBitmap(bitmap);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            Picasso.with(getApplicationContext()).load(selectedImage).into(profile_image);
+        }
     }
 
     private void updateUserDetail(){
@@ -58,6 +108,7 @@ public class Profile extends AppCompatActivity {
 
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                 .setDisplayName(text_display_name.getText().toString())
+                .setPhotoUri(selectedImage)
                 .build();
 
         user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
