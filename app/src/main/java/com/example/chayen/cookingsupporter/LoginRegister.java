f64ff7d410a1ab32c7d6861230ca7d1620c9ab50
chayenjr <chayenjr.sk131@gmail.com>
2017-02-09 15:33:51 +0700

    success on google signin on mobile device

--- app/src/main/java/com/example/chayen/cookingsupporter/LoginRegister.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/LoginRegister.java
@@ -1,15 +1,13 @@
 package com.example.chayen.cookingsupporter;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.nfc.Tag;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,8 +23,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -34,8 +30,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.net.Authenticator;
 
 public class LoginRegister extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -127,7 +121,7 @@ public class LoginRegister extends AppCompatActivity implements GoogleApiClient.
 
     protected void loginWithGooglePlus(){
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(this.getResources().getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
