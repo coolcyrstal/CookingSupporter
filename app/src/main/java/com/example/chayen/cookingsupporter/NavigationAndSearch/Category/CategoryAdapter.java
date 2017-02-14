24aba188b1a2986b037594f183fd1f30c2eacdec
coolcyrstal <chayenjr.sk131@gmail.com>
2017-02-15 01:28:22 +0700

    create method for connect adapter and category page

--- app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryAdapter.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryAdapter.java
@@ -1,8 +1,76 @@
 package com.example.chayen.cookingsupporter.NavigationAndSearch.Category;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.chayen.cookingsupporter.FoodListAdapter.FoodDatabaseClass;
+import com.example.chayen.cookingsupporter.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 /**
  * Created by chayen on 15-Feb-17.
  */
 
-public class CategoryAdapter {
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder>{
+
+    private MyClickListener mCallback;
+    private ArrayList<FoodDatabaseClass> mDataset;
+    private Context context;
+
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_category_food, parent, false);
+        ViewHolder dataObjHolder = new ViewHolder(view);
+        return dataObjHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryAdapter.ViewHolder holder, int position) {
+        holder.category_foodname.setText(mDataset.get(position).getFood_name());
+        Picasso.with(context).load(mDataset.get(position).getFood_image()).into(holder.category_foodimage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public void setOnItemClickListener(MyClickListener mCallback){
+        this.mCallback = mCallback;
+    }
+
+    public CategoryAdapter(ArrayList<FoodDatabaseClass> myDataset){
+        mDataset = myDataset;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView category_foodname;
+        ImageView category_foodimage;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            category_foodname = (TextView)itemView.findViewById(R.id.category_food_name);
+            category_foodimage = (ImageView)itemView.findViewById(R.id.category_food_image);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mCallback.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public interface MyClickListener{
+        public void onItemClick(int position, View v);
+    }
 }
--- app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryPage.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryPage.java
@@ -2,14 +2,51 @@ package com.example.chayen.cookingsupporter.NavigationAndSearch.Category;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import com.example.chayen.cookingsupporter.FoodListAdapter.FoodDatabaseClass;
 import com.example.chayen.cookingsupporter.R;
 
+import java.util.ArrayList;
+
 public class CategoryPage extends AppCompatActivity {
+
+    private CategoryAdapter myAdapter;
+    private RecyclerView recyclerView;
+    private ArrayList<FoodDatabaseClass> category_foodlist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_page);
+
+        initialize();
+    }
+
+    private void initialize(){
+        recyclerView = (RecyclerView)findViewById(R.id.category_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        myAdapter = new CategoryAdapter(category_foodlist);
+        getData();
+        recyclerView.setAdapter(myAdapter);
+    }
+
+    private void getData(){
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        ((CategoryAdapter) myAdapter).setOnItemClickListener(new CategoryAdapter.MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+
+            }
+        });
     }
 }
