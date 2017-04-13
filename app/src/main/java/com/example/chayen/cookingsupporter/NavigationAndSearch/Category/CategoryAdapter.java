15d2f6e6583eeb5cb03a696a41b508cf91e5845b
coolcyrstal <chayenjr.sk131@gmail.com>
2017-04-13 18:28:40 +0700

    add star rank method in category

--- app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryAdapter.java
+++ app/src/main/java/com/example/chayen/cookingsupporter/NavigationAndSearch/Category/CategoryAdapter.java
@@ -23,6 +23,19 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
     private MyClickListener mCallback;
     private ArrayList<FoodDatabaseClass> mDataset;
     private Context context;
+    private int[] image_star_rank = {
+            R.drawable.rsz_star_0,
+            R.drawable.rsz_star_0_5,
+            R.drawable.rsz_star_1,
+            R.drawable.rsz_star_1_5,
+            R.drawable.rsz_star_2,
+            R.drawable.rsz_star_2_5,
+            R.drawable.rsz_star_3,
+            R.drawable.rsz_star_3_5,
+            R.drawable.rsz_star_4,
+            R.drawable.rsz_star_4_5,
+            R.drawable.rsz_star_5
+    };
 
     @Override
     public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -38,6 +51,40 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         holder.category_foodname.setText(mDataset.get(position).getFood_name());
 //        Log.d("inheritfood testadapter", ""+ mDataset.get(0).getFood_name());
         Picasso.with(context).load(mDataset.get(position).getFood_image()).into(holder.category_foodimage);
+        Picasso.with(context).load(foodrank(position)).into(holder.category_foodrank);
+    }
+
+    private int foodrank(int position){
+        Double user_count, star_count, star_value;
+        user_count = mDataset.get(position).getUser_count().doubleValue();
+        star_count = mDataset.get(position).getStar_count().doubleValue();
+        if(user_count == 0 || star_count == 0){
+            return image_star_rank[0];
+        }
+        else{
+            star_value = user_count/star_count;
+            if(star_value <= 0.5){
+                return image_star_rank[1];
+            } else if(star_value > 0.5 && star_value <= 1){
+                return image_star_rank[2];
+            } else if(star_value > 1 && star_value <= 1.5){
+                return image_star_rank[3];
+            } else if(star_value > 1.5 && star_value <= 2){
+                return image_star_rank[4];
+            } else if(star_value > 2 && star_value <= 2.5){
+                return image_star_rank[5];
+            } else if(star_value > 2.5 && star_value <= 3){
+                return image_star_rank[6];
+            } else if(star_value > 3 && star_value <= 3.5){
+                return image_star_rank[7];
+            } else if(star_value > 3.5 && star_value <= 4){
+                return image_star_rank[8];
+            } else if(star_value > 4 && star_value <= 4.5){
+                return image_star_rank[9];
+            } else {
+                return image_star_rank[10];
+            }
+        }
     }
 
     @Override
@@ -58,11 +105,13 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
 
         private TextView category_foodname;
         private ImageView category_foodimage;
+        private ImageView category_foodrank;
 
         public ViewHolder(View itemView){
             super(itemView);
             category_foodname = (TextView)itemView.findViewById(R.id.category_food_name);
             category_foodimage = (ImageView)itemView.findViewById(R.id.category_food_image);
+            category_foodrank = (ImageView)itemView.findViewById(R.id.category_food_list_rank);
             itemView.setOnClickListener(this);
         }
 
